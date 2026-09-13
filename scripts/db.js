7838027711ffe4dd2db5ac6ed60ec7eb992059af
ysb000 <yusubeen0517@gmail.com>
2026-09-14 01:25:01 +0900

    css작성 중

---
+++ scripts/db.js
@@ -0,0 +1,22 @@
+//db.js
+//3행 액티비티 카테고리 데이터베이스
+const activityDb = [{
+    thum:'images/bnr/running.jpg',
+    title:'Running',
+},{
+    thum:'images/bnr/yoga.jpg',
+    title:'Yoga & Pilates',
+},{
+    thum:'images/bnr/daily.jpg',
+    title:'Daily wear',
+},{
+    thum:'images/bnr/golf.jpg',
+    title:'Golf',
+},{
+    thum:'images/bnr/tennis.jpg',
+    title:'Tennis',
+},{
+    thum:'images/bnr/training.jpg',
+    title:'Training',
+}
+];
