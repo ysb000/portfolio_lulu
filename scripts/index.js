559beb3e376c3ae5b518cd501a86f7d97f8afc7d
ysb000 <yusubeen0517@gmail.com>
2026-09-17 18:12:45 +0900

    css 정리

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 //index.js
 
 // =================================변수
-// =================================4행
+// =================================스와이퍼
 const best_woman_swiper = document.querySelector('.best_woman_swiper');
 const best_man_swiper = document.querySelector('.best_man_swiper');
 const best_acc_swiper = document.querySelector('.best_acc_swiper');
@@ -15,162 +15,37 @@ const daily_man_swiper = document.querySelector('.daily_man_swiper');
 
 const look_swiper = document.querySelector('.look_swiper');
 
+// =================================탭 메뉴
 const best_tab_menu = document.querySelectorAll('.best_tab_more .best_tab_menu > a');
+const running_tab_menu = document.querySelectorAll('.running_tab_menu > a');
+const daily_tab_menu = document.querySelectorAll('.daily_tab_menu > a');
+
+// =================================스와이프  컨텐츠
 const best_contents = document.querySelectorAll('.best_contents > .swiper');
+const running_contents = document.querySelectorAll('.running_contents > .swiper');
+const daily_contents = document.querySelectorAll('.daily_contents > .swiper');
 //================================= html 생성&반복문
 //================================= 4행 베스트셀러
-//여성
-for(let w of womanBestDb){
-    // console.log(w);
-    const best_woman_slide = document.createElement('div');
-    best_woman_slide.classList.add('swiper-slide');
-
-    best_woman_slide.innerHTML = `
-        <a href="#">
-            <p class="item_thum"><img src="${w.thum}" alt="${w.item_name}"></p>
-            <div class="item_contents">
-                <p class="item_name">${w.item_name}</p>
-                <p class="item_price">${w.item_price}</p>
-            </div>
-        </a>
-        <button type="button" class="wish_btn"><img src="./images/wish_v2.svg" alt="위시리스트"></button>
-
-    `;
-    best_woman_swiper.children[0].append(best_woman_slide);
-}
+// //여성
+slideFunc(womanBestDb, best_woman_swiper);
 //남성
-for(let m of manBestDb){
-    const best_man_slide = document.createElement('div');
-    best_man_slide.classList.add('swiper-slide');
-    
-    best_man_slide.innerHTML = `
-        <a href="#">
-            <p class="item_thum"><img src="${m.thum}" alt="${m.item_name}"></p>
-            <div class="item_contents">
-                <p class="item_name">${m.item_name}</p>
-                <p class="item_price">${m.item_price}</p>
-            </div>
-        </a>
-        <button type="button" class="wish_btn"><img src="./images/wish_v2.svg" alt="위시리스트"></button>
-    
-    `;
-    best_man_swiper.children[0].append(best_man_slide);
-}
+slideFunc(manBestDb, best_man_swiper);
 //액세서리
-for(let a of accBestDb){
-    const best_acc_slide = document.createElement('div');
-    best_acc_slide.classList.add('swiper-slide');
-    
-    best_acc_slide.innerHTML = `
-        <a href="#">
-            <p class="item_thum"><img src="${a.thum}" alt="${a.item_name}"></p>
-            <div class="item_contents">
-                <p class="item_name">${a.item_name}</p>
-                <p class="item_price">${a.item_price}</p>
-            </div>
-        </a>
-        <button type="button" class="wish_btn"><img src="./images/wish_v2.svg" alt="위시리스트"></button>
-    
-    `;
-    best_acc_swiper.children[0].append(best_acc_slide);
-}
-
+slideFunc(accBestDb, best_acc_swiper);
 //================================= 5행 러닝컬렉션
 //여성
-for(let w of woman_runningDB){
-    // console.log(w);
-    const running_woman_slide = document.createElement('div');
-    running_woman_slide.classList.add('swiper-slide');
-
-    running_woman_slide.innerHTML = `
-        <a href="#">
-            <p class="item_thum"><img src="${w.thum}" alt="${w.item_name}"></p>
-            <div class="item_contents">
-                <p class="item_name">${w.item_name}</p>
-                <p class="item_price">${w.item_price}</p>
-            </div>
-        </a>
-        <button type="button" class="wish_btn"><img src="./images/wish_v2.svg" alt="위시리스트"></button>
-
-    `;
-    running_woman_swiper.children[0].append(running_woman_slide);
-}
+slideFunc(woman_runningDB, running_woman_swiper);
 //남성
-for(let m of man_runningDB){
-    const running_man_slide = document.createElement('div');
-    running_man_slide.classList.add('swiper-slide');
-    
-    running_man_slide.innerHTML = `
-        <a href="#">
-            <p class="item_thum"><img src="${m.thum}" alt="${m.item_name}"></p>
-            <div class="item_contents">
-                <p class="item_name">${m.item_name}</p>
-                <p class="item_price">${m.item_price}</p>
-            </div>
-        </a>
-        <button type="button" class="wish_btn"><img src="./images/wish_v2.svg" alt="위시리스트"></button>
-    
-    `;
-    running_man_swiper.children[0].append(running_man_slide);
-}
+slideFunc(man_runningDB, running_man_swiper);
 //액세서리
-for(let a of acc_runningDB){
-    const running_acc_slide = document.createElement('div');
-    running_acc_slide.classList.add('swiper-slide');
-    
-    running_acc_slide.innerHTML = `
-        <a href="#">
-            <p class="item_thum"><img src="${a.thum}" alt="${a.item_name}"></p>
-            <div class="item_contents">
-                <p class="item_name">${a.item_name}</p>
-                <p class="item_price">${a.item_price}</p>
-            </div>
-        </a>
-        <button type="button" class="wish_btn"><img src="./images/wish_v2.svg" alt="위시리스트"></button>
-    
-    `;
-    running_acc_swiper.children[0].append(running_acc_slide);
-}
-
+slideFunc(acc_runningDB, running_acc_swiper);
 //================================= 7행 데일리웨어
 //여성
-for(let w of womanDailyDb){
-    const daily_woman_slide = document.createElement('div');
-    daily_woman_slide.classList.add('swiper-slide');
-
-    daily_woman_slide.innerHTML = `
-    <a href="#">
-        <p class="item_thum"><img src="${w.thum}" alt="${w.item_name}"></p>
-        <div class="item_contents">
-            <p class="item_name">${w.item_name}</p>
-            <p class="item_price">${w.item_price}</p>
-            </div>
-        </a>
-        <button type="button" class="wish_btn"><img src="./images/wish_v2.svg" alt="위시리스트"></button>
-    </a>
-    `;
-    daily_woman_swiper.children[0].append(daily_woman_slide);
-}
+slideFunc(womanDailyDb, daily_woman_swiper);
 //남성
-for(let m of manDailyDb){
-    const daily_man_slide = document.createElement('div');
-    daily_man_slide.classList.add('swiper-slide');
-    
-    daily_man_slide.innerHTML = `
-        <a href="#">
-            <p class="item_thum"><img src="${m.thum}" alt="${m.item_name}"></p>
-            <div class="item_contents">
-                <p class="item_name">${m.item_name}</p>
-                <p class="item_price">${m.item_price}</p>
-            </div>
-        </a>
-        <button type="button" class="wish_btn"><img src="./images/wish_v2.svg" alt="위시리스트"></button>
-    
-    `;
-    daily_man_swiper.children[0].append(daily_man_slide);
-}
+slideFunc(manDailyDb, daily_man_swiper);
 
-//9행 look_swiper
+//=================================9행 look_swiper
 for(let l of snsDb){
     const look_slide = document.createElement('div');
     look_slide.classList.add('swiper-slide');
@@ -187,13 +62,11 @@ for(let l of snsDb){
 
     look_swiper.children[0].append(look_slide);
 }
-//=================================스와이퍼 연결
+//=============================================================스와이퍼 연결
 //================================= 4행
 const best_woman_swiper_func = new Swiper(best_woman_swiper, {
     slidesPerView:5, //보이는 개수
     spaceBetween:30, //여백
-    observer: true,        // DOM 변화 감지 후 재계산
-    observeParents: true,  // 부모 요소 변화 감지 후 재계산
 }); //swiper 플러그인 함수 최종 연결
 const best_man_swiper_func = new Swiper(best_man_swiper, {
     slidesPerView:5, //보이는 개수
@@ -242,25 +115,58 @@ const look_swiper_func = new Swiper(look_swiper, {
     spaceBetween:20, //여백
 }); //swiper 플러그인 함수 최종 연결
 
-//================================= 함수 
+//============================================================= 이벤트
+//탭 클릭 시 상품 레이아웃 변경
+//베스트셀러
+tabEventFunc(best_tab_menu, best_contents, 
+    [best_woman_swiper_func, best_man_swiper_func, best_acc_swiper_func]);
+tabEventFunc(running_tab_menu, running_contents, 
+    [running_woman_swiper_func, running_man_swiper_func, running_acc_swiper_func]);
+tabEventFunc(daily_tab_menu, daily_contents, [daily_woman_swiper_func, daily_man_swiper_func]);
+
+//============================================================= 함수
+//swiper 생성 함수
+function slideFunc(db, swiper){
+    for(let v of db){
+        const slide = document.createElement('div');
+        slide.classList.add('swiper-slide');
+
+        slide.innerHTML = `
+            <a href="#">
+                <p class="item_thum"><img src="${v.thum}" alt="${v.item_name}"></p>
+                <div class="item_contents">
+                    <p class="item_name">${v.item_name}</p>
+                    <p class="item_price">${v.item_price}</p>
+                </div>
+            </a>
+            <button type="button" class="wish_btn"><img src="./images/wish_v2.svg" alt="위시리스트"></button>
+        `;
+        swiper.children[0].append(slide);
+    }
+}
+
 //reset함수
 function resetFunc(target){
     for(let reset of target){
         reset.classList.remove('active');
     }
 }
+//탭 클릭 시 상품 변경 함수
+function tabEventFunc(tabB, itemC, swiperList= []){
+    tabB.forEach((o, i)=>{
+        // console.log(o, i);
+        o.addEventListener('click',(e)=>{
+            e.preventDefault(); //a태그 기본 링크 이동 방지
+            
+            resetFunc(tabB);
+            o.classList.add('active');
+    
+            resetFunc(itemC);
+            itemC[i].classList.add('active');
 
-//================================= 이벤트
-//탭 클릭 시 상품 레이아웃 변경
-//베스트셀러
-best_tab_menu.forEach((o, i)=>{
-    // console.log(o, i);
-    o.addEventListener('click',()=>{
-        resetFunc(best_tab_menu);
-        o.classList.add('active');
-        console.log(o,i);
-
-        resetFunc(best_contents);
-        best_contents[i].classList.add('active');
-    })
-})
+            if(swiperList[i]){
+                swiperList[i].update();
+            }
+        });
+    });
+}
